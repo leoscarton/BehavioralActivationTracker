d4596f085383d8cf08bfc5683f50617174368c55
leoscarton <lfscarton@gmail.com>
2026-07-21 14:19:37 -0300

    Added simple verifications for attributes inside Activity class

--- src-python/domain/entities/activity.py
+++ src-python/domain/entities/activity.py
@@ -14,7 +14,26 @@ class Activity:
     mood_after: float
 
     def __post_init__(self):
-        # Subject to change later in the project
+        # All verifications are subject to change later in the project
+        if not self.title or len(self.title) <= 0:
+            raise ValueError("Title cannot be empty")
+
+        # There will be eventually a check to see if the category exists in the database
+        if not self.category or len(self.category) <= 0:
+            raise ValueError("Category cannot be empty")
+
+        if not self.description or len(self.description) <= 0:
+            self.description = ""
+
+        if not self.planned_date_of_completion:
+            raise ValueError("Planned date of completion cannot be empty")
+
+        if (self.actual_date_of_completion is not None) and (self.actual_date_of_completion < self.planned_date_of_completion):
+            raise ValueError("Actual date of completion cannot be before planned date of completion")
+
+        if self.status is None:
+            self.status = False
+
         if not ((0.0 <= self.mood_before <= 10.0) and (0.0 <= self.mood_after <= 10.0)):
             raise ValueError("Mood values must range from 0.0 to 10.0")
         
